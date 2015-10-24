61fc36e9624ebaa6c23afb4a89ef687875ba72dc
rubinmd <matt@Matthews-MacBook-Pro.local>
2015-10-24 14:09:03 -0500

    Dynamic
    All except for DB

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,6 +6,41 @@ class GameController < ApplicationController
 
   # Your code goes below.
   def rps
-  end
+    @user_move=params[:the_move]
+    @computer_move=["rock","paper","scissors"].sample
 
+    if @user_move=="rock"
+      if @computer_move==@user_move
+        @result="tied"
+      end
+      if @computer_move=="paper"
+        @result="lose"
+      end
+      if @computer_move=="scissors"
+        @result="win"
+      end
+    end
+    if @user_move=="paper"
+      if @computer_move==@user_move
+        @result="tied"
+      end
+      if @computer_move=="rock"
+        @result="win"
+      end
+      if @computer_move=="scissors"
+        @result="lose"
+      end
+    end
+    if @user_move=="scissors"
+      if @computer_move==@user_move
+        @result="tied"
+      end
+      if @computer_move=="rock"
+        @result="lose"
+      end
+      if @computer_move=="paper"
+        @result="win"
+      end
+    end
+  end
 end
